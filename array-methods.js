a54c68b735c84aa0b85ba564eca8f71a8ecae17c
Russell Ries <riesru@gmail.com>
2016-04-25 09:04:30 -0500

    add map method

--- array-methods.js
+++ array-methods.js
@@ -7,8 +7,16 @@ var planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus
 var el = document.getElementById("planets");
 
 // Use the map method to create a new array where the first letter of each planet is capitalized
+var capPlanets = planets.map(function(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+})
+console.log("capPlanets = ", capPlanets);
 
 // Use the filter method to create a new array that contains planets with the letter 'e'
+var ePlanets = planets.filter(function(yesE) {
+  return yesE.indexOf("e") === ;
+})
+console.log("ePlanets = ", ePlanets);
 
 // Use the reduce method to create a sentence from the words in the following array
 
